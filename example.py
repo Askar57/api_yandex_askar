5ece2ab59c6d6f54ba61de165a15b80327551499
Askar57 <58077922+Askar57@users.noreply.github.com>
2021-02-01 17:02:44 +0500

    Add files via upload

--- example.py
+++ example.py
@@ -1,11 +1,7 @@
 import os
-import sys
-
 import requests
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-
-SCREEN_SIZE = [450, 450]
 
 
 class Example(QWidget):
@@ -24,28 +20,30 @@ class Example(QWidget):
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
 
-        # Запишем полученное изображение в файл.
+        # Полученное изображение конвертируем в файл.
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
 
     def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setGeometry(500, 200, 450, 450)
         self.setWindowTitle('Отображение карты')
 
-        ## Изображение
+        # Изображение
         self.pixmap = QPixmap(self.map_file)
         self.image = QLabel(self)
         self.image.move(0, 0)
-        self.image.resize(600, 450)
+        self.image.resize(450, 450)
         self.image.setPixmap(self.pixmap)
 
+    # Закрываем и удаляем png файл
     def closeEvent(self, event):
-        """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
 
 
 if __name__ == '__main__':
+    import sys
+
     app = QApplication(sys.argv)
     ex = Example()
     ex.show()
