0780f2a883fcd0c490f52747140d1fee003dc181
Askar57 <58077922+Askar57@users.noreply.github.com>
2021-02-01 16:33:23 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-import PyQt5
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QApplication
 
 
 class QMainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("LoginWindow")
         MainWindow.resize(800, 600)
+
+    def keyPressEvent(self, buttonUpDown):
+
+        if buttonUpDown.key() == Qt.Key_PageDown:
+            self.______()
+
+        if buttonUpDown.key() == Qt.Key_PageUp:
+            self.______()
 
 
 if __name__ == "__main__":
